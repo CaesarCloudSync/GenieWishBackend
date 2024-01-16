dcadc02dadd8ddc24cb019e27baf279162bb94fa
amari <amari.lawal@gmail.com>
2024-01-16 22:20:44 +0000

    genie wish start

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "$1"
-git push 
+git push origin -u main:main
 docker build -t palondomus/geniewishbackend:latest .
-#docker push palondomus/geniewishbackend:latest 
+docker push palondomus/geniewishbackend:latest 
 docker run -it -p 8080:8080 palondomus/geniewishbackend:latest
